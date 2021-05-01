bf22e476beb7f5efaf10bb03f369b8ab7776f6c7
ugurtan <pektas.ugurtan@gmail.com>
2021-05-01 20:51:17 +0300

    products and banners completed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+import '../assets/scss/banner.scss';
+
+import bannerOne from '../assets/img/banner-1.png';
+import bannerTwo from '../assets/img/banner-2.png';
+import bannerThree from '../assets/img/banner-3.png';
+
 class Banner extends React.Component{
 
     componentDidMount(){
@@ -8,11 +14,52 @@ class Banner extends React.Component{
 
     render(){
        return (
-            <div className="row banner-container">
-                <div className="col-12">
-                    Banner
-                </div>
-            </div>
+           <div className="banner-container">
+               <div className="container">
+                    <div className="row">
+                        <div className="col-lg-4 col-md-12 col-sm-12">
+                            <div className="banner-item red">
+                                <div className="banner-image">
+                                    <img src={bannerOne} alt="Banner One" />
+                                </div>
+                                <div className="banner-content">
+                                    <div>75 TL Üzerine Teslimat Ücreti Bizden</div>
+                                    <div>
+                                        <a href="javascript:;" className="detail-button">Detaylı Bilgi</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-12 col-sm-12">
+                            <div className="banner-item blue">
+                                <div className="banner-image">
+                                    <img src={bannerTwo} alt="Banner Two" />
+                                </div>
+                                <div className="banner-content">
+                                    <div>Hediye Kategorisi için Sepette %15 İndirim</div>
+                                    <div>
+                                        <a href="javascript:;" className="detail-button">Hediye Ürünleri</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-12 col-sm-12">
+                            <div className="banner-item green">
+                                <div className="banner-image">
+                                    <img src={bannerThree} alt="Banner Three" />
+                                </div>
+                                <div className="banner-content">
+                                    <div>Kırtasiye Kategorisi için Sepette %15 İndirim</div>
+                                    <div>
+                                        <a href="javascript:;" className="detail-button">Detaylı Bilgi</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+               </div>
+           </div>
+            
         )
     }
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,8 @@
 import React from "react";
+import '../assets/scss/products.scss';
 
+import productsHeaderIcon from '../assets/img/products-header-icon.svg';
+import productOne from '../assets/img/product-1.png';
 class Products extends React.Component{
 
     componentDidMount(){
@@ -8,11 +11,177 @@ class Products extends React.Component{
 
     render(){
        return (
-            <div className="row products-container">
-                <div className="col-12">
-                    Products
+           <div className="container products-container">
+               <div className="row">
+                   <div className="col-lg-12 col-md-12 col-sm-12">
+                       <h2><img src={productsHeaderIcon} alt="Ürünler" /> Tüm Kategoriler</h2>
+                   </div>
+               </div>
+                <div className="row row-5th">
+                    <div className="col-5th">
+                        <div className="product">
+                            <div className="product-top">
+                                <img src={productOne} alt="Product One" />
+                                <div className="product-name">
+                                    Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth
+                                </div>
+                            </div>
+                            <div className="product-bottom">
+                                <div className="free-cargo">Ücretsiz Teslimat</div>
+                                <div className="price">399,99 TL</div>
+                                <div className="add-to-cart">
+                                    <button>Sepete Ekle</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-5th">
+                        <div className="product">
+                            <div className="product-top">
+                                <img src={productOne} alt="Product One" />
+                                <div className="product-name">
+                                    Z6 Akıllı Saat Kameralı Konuşma 
+                                </div>
+                            </div>
+                            <div className="product-bottom">
+                                <div className="price">399,99 TL</div>
+                                <div className="add-to-cart">
+                                    <button>Sepete Ekle</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-5th">
+                        <div className="product">
+                            <div className="product-top">
+                                <img src={productOne} alt="Product One" />
+                                <div className="product-name">
+                                    Z6 Akıllı Saat Kameralı Konuşma Garantili Sim 
+                                </div>
+                            </div>
+                            <div className="product-bottom">
+                                <div className="free-cargo">Ücretsiz Teslimat</div>
+                                <div className="price">399,99 TL</div>
+                                <div className="add-to-cart">
+                                    <button>Sepete Ekle</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-5th">
+                        <div className="product">
+                            <div className="product-top">
+                                <img src={productOne} alt="Product One" />
+                                <div className="product-name">
+                                    Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth Telefon kulaklık saat
+                                </div>
+                            </div>
+                            <div className="product-bottom">
+                                <div className="price">399,99 TL</div>
+                                <div className="add-to-cart">
+                                    <button>Sepete Ekle</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-5th">
+                        <div className="product">
+                            <div className="product-top">
+                                <img src={productOne} alt="Product One" />
+                                <div className="product-name">
+                                    Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth Telefon kulaklık saat
+                                </div>
+                            </div>
+                            <div className="product-bottom">
+                                <div className="price">399,99 TL</div>
+                                <div className="add-to-cart">
+                                    <button>Sepete Ekle</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-5th">
+                        <div className="product">
+                            <div className="product-top">
+                                <img src={productOne} alt="Product One" />
+                                <div className="product-name">
+                                    Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth Telefon kulaklık saat
+                                </div>
+                            </div>
+                            <div className="product-bottom">
+                                <div className="price">399,99 TL</div>
+                                <div className="add-to-cart">
+                                    <button>Sepete Ekle</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-5th">
+                        <div className="product">
+                            <div className="product-top">
+                                <img src={productOne} alt="Product One" />
+                                <div className="product-name">
+                                    Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth Telefon kulaklık saat
+                                </div>
+                            </div>
+                            <div className="product-bottom">
+                                <div className="price">399,99 TL</div>
+                                <div className="add-to-cart">
+                                    <button>Sepete Ekle</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-5th">
+                        <div className="product">
+                            <div className="product-top">
+                                <img src={productOne} alt="Product One" />
+                                <div className="product-name">
+                                    Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth Telefon kulaklık saat
+                                </div>
+                            </div>
+                            <div className="product-bottom">
+                                <div className="price">399,99 TL</div>
+                                <div className="add-to-cart">
+                                    <button>Sepete Ekle</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-5th">
+                        <div className="product">
+                            <div className="product-top">
+                                <img src={productOne} alt="Product One" />
+                                <div className="product-name">
+                                    Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth Telefon kulaklık saat
+                                </div>
+                            </div>
+                            <div className="product-bottom">
+                                <div className="price">399,99 TL</div>
+                                <div className="add-to-cart">
+                                    <button>Sepete Ekle</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-5th">
+                        <div className="product">
+                            <div className="product-top">
+                                <img src={productOne} alt="Product One" />
+                                <div className="product-name">
+                                    Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth Telefon kulaklık saat
+                                </div>
+                            </div>
+                            <div className="product-bottom">
+                                <div className="price">399,99 TL</div>
+                                <div className="add-to-cart">
+                                    <button>Sepete Ekle</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+           </div>
         )
     }
 }
