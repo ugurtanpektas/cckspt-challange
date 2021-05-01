0846531acc987a234886b97cf1b6554bc778f7e7
ugurtan <pektas.ugurtan@gmail.com>
2021-05-01 19:26:01 +0300

    filters created

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,4 +1,6 @@
 import React from "react";
+import '../assets/scss/filter.scss';
+import categoryIcon from '../assets/img/category-icon.svg';
 
 class Filter extends React.Component{
 
@@ -8,11 +10,51 @@ class Filter extends React.Component{
 
     render(){
        return (
-            <div className="row filter-container">
-                <div className="col-12">
-                    Filter
+           <div className="container filter-container">
+               <div className="row">
+                    <div className="col-12">
+                        <h2><img src={categoryIcon} alt="Kategoriler" /> Kategoriler</h2>
+                    </div>
                 </div>
-            </div>
+                <div className="row">
+                    <div className="col-lg-2">
+                        <a href="#" className="filter-item active">Tüm Kategoriler</a>
+                    </div>
+                    <div className="col-lg-2">
+                        <a href="#" className="filter-item">Elektronik</a>
+                    </div>
+                    <div className="col-lg-2">
+                        <a href="#" className="filter-item">Ev ve Yaşam</a>
+                    </div>
+                    <div className="col-lg-2">
+                        <a href="#" className="filter-item">Evcil Hayvan</a>
+                    </div>
+                    <div className="col-lg-2">
+                        <a href="#" className="filter-item">Kitap</a>
+                    </div>
+                    <div className="col-lg-2">
+                        <a href="#" className="filter-item">Oyuncak</a>
+                    </div>
+                    <div className="col-lg-2">
+                        <a href="#" className="filter-item">Spor</a>
+                    </div>
+                    <div className="col-lg-2">
+                        <a href="#" className="filter-item">Çiçek (120)</a>
+                    </div>
+                    <div className="col-lg-2">
+                        <a href="#" className="filter-item">Hediye</a>
+                    </div>
+                    <div className="col-lg-2">
+                        <a href="#" className="filter-item">Moda, Aksesuar</a>
+                    </div>
+                    <div className="col-lg-2">
+                        <a href="#" className="filter-item">Ofis, Kırtasiye</a>
+                    </div>
+                    <div className="col-lg-2">
+                        <a href="#" className="filter-item">Parfüm</a>
+                    </div>
+                </div>
+           </div>
         )
     }
 }
